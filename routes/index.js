c596d4a7e067747c7b5a57601b3237938d2389ac
jvfe <jvfecav@gmail.com>
2021-10-12 15:54:43 -0300

    feat: Done basic server logic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Mini messageboard", messages: messages });
+});
+
+router.post("/new", function (req, res, next) {
+  const newMessage = {
+    text: req.body.description,
+    user: req.body.username,
+    added: new Date(),
+  };
+
+  messages.push(newMessage);
+
+  res.redirect("/");
 });
 
 module.exports = router;
